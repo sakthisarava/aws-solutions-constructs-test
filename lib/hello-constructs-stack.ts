17718658f75080cacc642191c3975a97184de70f
sakthisarava <velsakthis5301@gmail.com>
2022-06-01 17:45:28 +0530

    first commit

--- lib/hello-constructs-stack.ts
+++ lib/hello-constructs-stack.ts
@@ -1,6 +1,11 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as api from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { ApiGatewayToLambda, ApiGatewayToLambdaProps } from '@aws-solutions-constructs/aws-apigateway-lambda';
+import { LambdaToDynamoDB, LambdaToDynamoDBProps } from '@aws-solutions-constructs/aws-lambda-dynamodb';
 
 export class HelloConstructsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +13,44 @@ export class HelloConstructsStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloConstructsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // hello function responding to http requests
+    const helloFunc = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    // hit counter, aws-lambda-dynamodb pattern
+    const lambda_ddb_props: LambdaToDynamoDBProps = {
+      lambdaFunctionProps: {
+        code: lambda.Code.fromAsset(`lambda`),
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: 'hitcounter.handler',
+        environment: {
+          DOWNSTREAM_FUNCTION_NAME: helloFunc.functionName
+        }
+      },
+      dynamoTableProps: {
+        tableName: 'SolutionsConstructsHits',
+        partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+        removalPolicy: RemovalPolicy.DESTROY
+      }
+    };
+
+    const hitcounter = new LambdaToDynamoDB(this, 'LambdaToDynamoDB', lambda_ddb_props);
+
+    // grant the hitcounter lambda role invoke permissions to the hello function
+    helloFunc.grantInvoke(hitcounter.lambdaFunction);
+
+    const api_lambda_props: ApiGatewayToLambdaProps = {
+      existingLambdaObj: hitcounter.lambdaFunction,
+      apiGatewayProps: {
+        defaultMethodOptions: {
+          authorizationType: api.AuthorizationType.IAM
+        }
+      }
+    };
+
+    new ApiGatewayToLambda(this, 'ApiGatewayToLambda', api_lambda_props);
   }
 }
